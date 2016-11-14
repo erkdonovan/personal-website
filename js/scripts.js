2d0a74fc4f72f8ddd9f5289ab0aec72b2f62f0ac
Erika Donovan <erkdonovan@gmail.com>
2016-11-14 15:09:27 -0500

    changes to nav hover colour

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ $(function(){
 
   //slide-out nav
     $('.display__mobile i').on('click', function() {
-      console.log('hi');
         $('.display__mobile--menu').toggleClass('nav-open');
         $('.display__mobile i').toggleClass('fa-times').toggleClass('fa-bars');
     });
